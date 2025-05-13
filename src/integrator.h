de34a99afd13ca10e8b6bf3ae00aa8482e80627e
MFBarker <98473836+MFBarker@users.noreply.github.com>
2025-05-12 20:14:35 -0600

    [E] Euler Integration

--- src/integrator.h
+++ src/integrator.h
@@ -11,7 +11,7 @@ inline void ExplicitIntegrator(Body& body, float timestep)
 
 inline void SemiImplicitIntegrator(Body& body, float timestep)
 {
-	body.position += (body.velocity * timestep);
 	body.velocity += (body.acceleration * timestep);
+	body.position += (body.velocity * timestep);
 	body.velocity *= 1.0f / (1.0f + body.damping * timestep);
 }
